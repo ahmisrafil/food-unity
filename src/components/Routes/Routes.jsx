2f472bd71479eae886acd544fb47114c037a67ea
ahmisrafil <ahmisrafil@yahoo.com>
2023-11-07 01:15:48 +0600

    basic route setup and error page added

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+import Error from "../../pages/Error/Error";
+import Home from "../../pages/Home/Home";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root />,
+      errorElement: <Error></Error>,
+      children: [
+        {
+          path:"/",
+          element: <Home></Home>
+        },
+        // {
+        //   path:"/",
+        //   element: 
+        // },
+        // {
+        //   path:"/",
+        //   element: 
+        // },
+        // {
+        //   path:"/",
+        //   element: 
+        // },
+      ]
     },
   ]);
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className="text-center text-8xl font-bold">Welcome to home</h3>
+            
+        </div>
+    );
+};
+
+export default Home;
