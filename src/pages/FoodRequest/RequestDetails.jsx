4b334a9bfbd85a9b897006362bc99d5fd2a0aedc
ahmisrafil <ahmisrafil@yahoo.com>
2023-11-16 08:45:26 +0600

    cancel request handled with delete operation

--- src/pages/FoodRequest/RequestDetails.jsx
+++ src/pages/FoodRequest/RequestDetails.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-
+import Swal from 'sweetalert2'
 
 const RequestDetails = ({ food }) => {
 
@@ -9,7 +9,35 @@ const RequestDetails = ({ food }) => {
 
 
     const handleCancle = (_id) => {
+        console.log('deleted');
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, cancel it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
 
+                fetch(`http://localhost:5000/request/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Cancelled!',
+                                'Your request has been cancelled.',
+                                'success'
+                            )
+                        }
+                    })
+
+            }
+        })
     }
 
 
